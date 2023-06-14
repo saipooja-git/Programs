2a4ba57a201b0cadc1e438f70519e1cd18c27d11
saipooja-git <pallesaipooja@gmail.com>
2023-06-14 12:37:12 +0530

    commit

---
+++ java_new_programs/src/java_new_programs/NumberIncreasingPyramid.java
@@ -0,0 +1,26 @@
+package java_new_programs;
+
+public class NumberIncreasingPyramid {
+	
+	public static void numberIncrement(int n)
+	{
+		int i,j;
+		for(i = 1;i<=n;i++)
+			{
+			for(j =1 ;j<=i;j++)
+			{
+				System.out.print(j + " ");
+			}
+			System.out.println();
+			}
+	}
+
+
+	public static void main(String[] args) {
+		int n=5;
+		numberIncrement(n);
+
+	}
+}
+
+
